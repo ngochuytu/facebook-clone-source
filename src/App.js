4606910be897545f0730652d6805eb9258ff2a65
ngochuytu <tungochuy2001@gmail.com>
2021-12-25 17:25:31 +0700

    Update responsive styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-// import { HashRouter as Router, Switch, Route } from 'react-router-dom'; //Github pages
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'; //Github pages
 import { NotificationsProvider } from "./Contexts/NotificationsContext";
 import { useFireBaseAuthContext } from './Contexts/FireBaseAuthContext';
 import { Redirect } from 'react-router-dom';
