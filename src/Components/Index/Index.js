bd0740cb839ab010cd3884526dc14f8b7e00cad2
ngochuytu <tungochuy2001@gmail.com>
2022-01-20 18:03:10 +0700

    Add: Page Title

--- src/Components/Index/Index.js
+++ src/Components/Index/Index.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { RefetchPostsContextProvider } from "../../Contexts/RefetchPostsContext";
 import Header from "../Header/Header";
 import Main from "./Main/Main";
 
 function Index() {
+    useEffect(() => {
+        document.title = "Facebook";
+    }, []);
+
     return (
         <div>
             <RefetchPostsContextProvider>
