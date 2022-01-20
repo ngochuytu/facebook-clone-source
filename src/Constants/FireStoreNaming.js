4fb44dbac1ee3eec74219f63cff95f4d562c5cd3
ngochuytu <tungochuy2001@gmail.com>
2022-01-20 17:51:06 +0700

    Add: FirestoreNaming: Merge collectionName and documentName variables

--- src/Constants/FireStoreNaming.js
+++ src/Constants/FireStoreNaming.js
@@ -1,12 +1,92 @@
-export const collectionNames = {
-    posts: "posts",
-    comments: "comments",
-    interactions: "interactions",
-    notifications: "notifications",
-    notificationsList: "notificationsList",
-    users: "users"
-};
-
-export const documentNames = {
-    notificationsDetails: "notificationsDetails",
+export const firebaseCollections = {
+    users: {
+        collectionName: "users",
+        documents: {
+            documentField: {
+                displayName: "displayName",
+                photoURL: "photoURL",
+                uid: "uid"
+            }
+        },
+        subCollections: {
+            notifications: {
+                collectionName: "notifications",
+                documents: {
+                    notificationsDetails: {
+                        documentName: "notificationsDetails",
+                        documentField: {
+                            numbersOfNewNotifications: "numbersOfNewNotifications"
+                        }
+                    }
+                },
+                subCollections: {
+                    notificationsList: {
+                        collectionName: "notificationsList",
+                        documents: {
+                            documentField: {
+                                commentContent: "commentContent",
+                                id: "id",
+                                interactedUid: "interactedUid",
+                                interactionType: "interactionType",
+                                postId: "postId",
+                                read: "read",
+                                timeStamp: "timeStamp"
+                            }
+                        }
+                    }
+                }
+            },
+            about: {
+                collectionName: "about",
+                documents: {
+                    overview: {
+                        documentName: "overview",
+                        documentField: {
+                            workplace: "workplace",
+                            highSchool: "highSchool",
+                            university: "university",
+                            currentCity: "currentCity",
+                            hometown: "hometown"
+                        }
+                    }
+                }
+            }
+        }
+    },
+    posts: {
+        collectionName: "posts",
+        documents: {
+            documentField: {
+                attachmentFullPath: "attachmentFullPath",
+                attachmentPreviewURL: "attachmentPreviewURL",
+                content: "content",
+                id: "id",
+                timeStamp: "timeStamp",
+                uid: "uid"
+            }
+        },
+        subCollections: {
+            interactions: {
+                collectionName: "interactions",
+                documents: {
+                    documentField: {
+                        interactionType: "interactionType",
+                        timeStamp: "timeStamp",
+                        uid: "uid"
+                    }
+                }
+            },
+            comments: {
+                collectionName: "comments",
+                documents: {
+                    documentField: {
+                        content: "content",
+                        id: "id",
+                        timeStamp: "timeStamp",
+                        uid: "uid"
+                    }
+                }
+            }
+        }
+    },
 };
